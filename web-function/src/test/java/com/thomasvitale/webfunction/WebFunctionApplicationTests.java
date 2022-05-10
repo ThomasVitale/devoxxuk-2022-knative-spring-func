9d87341adc0574e2aed8d72131b0620d9c0e8d0d
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-05-10 09:15:00 +0100

    Implement function

--- web-function/src/test/java/com/thomasvitale/webfunction/WebFunctionApplicationTests.java
+++ web-function/src/test/java/com/thomasvitale/webfunction/WebFunctionApplicationTests.java
@@ -1,13 +1,24 @@
 package com.thomasvitale.webfunction;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebFunctionApplicationTests {
 
+    @Autowired
+    private WebTestClient webClient;
+
     @Test
-    void contextLoads() {
+    void testFunctionDefinitionApi() {
+        String expectedSkill = "I can play the PIANO";
+        webClient.post()
+                .bodyValue("piano")
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo(expectedSkill);
     }
 
 }
